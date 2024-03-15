bbce8e913b17cd12bbd8e447aad418cf49f2891b
j.jauregui@uniandes.edu.co <j.jauregui@uniandes.edu.co>
2024-03-15 09:40:47 -0500

    Cambios en index.js autorizados

--- parcial1/src/index.js
+++ parcial1/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { IntlProvider } from 'react-intl';
+import localeEsMessages from './locales/es'; 
+import localeEnMessages from './locales/en'; 
 
-ReactDOM.render(
-  <React.StrictMode>
+const userLanguage = navigator.language || navigator.userLanguage;
+const messages = userLanguage.startsWith('es') ? localeEsMessages : localeEnMessages;
+const locale = userLanguage;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <IntlProvider locale={locale} messages={messages}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </IntlProvider>, 
 );
 
 // If you want to start measuring performance in your app, pass a function
